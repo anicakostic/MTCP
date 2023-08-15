4f5ac2f206be3a93c5f3dc2cac15f95d0750c29a
Anica Kostic <anicakostic@Anicas-MacBook-Pro.local>
2023-08-15 15:16:09 +0100

    realdata and numerical

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' Change-point locations for alpha = 1, for the Gaussian model
+#'
+#' This data contains the locations of the true change-points
+#' for the DOS statistic with alpha = 1, under the Gaussian model,
+#' for various values of mu (columns) and pi1 (rows).
+#' This data is obtained using Mathematica.
+#'
+"CP_loc_alpha1"
+
+#' Estimable proportion for alpha = 1, for the Gaussian model
+#'
+#' This data contains the values corresponding to the part of the
+#' false null proportion that the DOS method with alpha = 1
+#' is able to estimate asymptotically.
+#' This data is obtained using Mathematica.
+#'
+"estimable_prop_slope"
+
+
+#' Change-point locations for alpha = 1/2, for the Gaussian model
+#'
+#' This data contains the locations of the true change-points
+#' for the DOS statistic with alpha = 1, under the Gaussian model,
+#' for various values of mu (columns) and pi1 (rows).
+#' This data is obtained using Mathematica.
+#'
+"CP_loc_alpha05"
+
+
+#' Estimable proportion for alpha = 1/2, for the Gaussian model
+#'
+#' This data contains the values corresponding to the part of the
+#' false null proportion that the DOS method with alpha = 1/2
+#' is able to estimate asymptotically.
+#' This data is obtained using Mathematica.
+#'
+"estimable_prop_sqrt"
+
+
+
